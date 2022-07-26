381a13aa341c31201bc832226d4867f902abad1f
Thiago da Costa <thiago88c@gmail.com>
2022-07-26 12:13:55 -0300

    incluindo atividade remota 1

---
+++ atividaderemota1/atividade.js
@@ -0,0 +1,19 @@
+let PesoPecas = [ PesoPecas >= 100];
+console.log(PesoPecas);
+let quantidadecaixa = [quantidadecaixa <= 10];
+console.log(quantidadecaixa);
+let quantidadecaracteres = [quantidadecaracteres.length > 3];
+{if (PesoPecas >= 100("produto cadastrado com sucesso") );
+else {
+    console.log("cadastro negado peso incorreto");
+}
+{if (quantidadecaixa <= 10("pedido finalizado com sucesso"));
+else {
+    console.log(("quantidade excede o limite da caixa"));
+}
+{if (quantidadecaracteres.length > 3);
+    else ("Não é permitido esse nome");
+    
+}
+}
+}
