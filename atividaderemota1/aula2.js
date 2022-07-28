80fa7d9c406ad01e109bbc5704efd19ebac67fb4
Thiago da Costa <thiago88c@gmail.com>
2022-07-28 00:18:05 -0300

    atividade remota 1

---
+++ atividaderemota1/aula2.js
@@ -0,0 +1,22 @@
+let peso = 150
+
+
+if (peso>100) {
+    console.log('pode cadastrar')
+} else {
+    console.log('Peso invalido')
+}
+// --------------------------------------
+let listaPecas = 12
+if (listaPecas>10){
+    console.log('limite da caixa ultrapassado')
+}else {
+    console.log('pode cadastrar')
+}
+//------------------------------------------------
+let nomePeca = 'carburador'
+if (nomePeca.length > 3) {
+    console.log('pode cadastrar')
+} else {
+    console.log('nome invalido')
+}
---
+++ atividaderemota1/let numeroDeAlunos = 10.js
@@ -0,0 +1,6 @@
+let numeroDeAlunos = 10
+
+for (let contador  = 0; contador  < numeroDeAlunos.length; contador ++) {
+   console.log(contador)
+    
+}
